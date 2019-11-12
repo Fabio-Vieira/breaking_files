78372c2d26be7d5596415468d16a2e31be392d93
Fábio Vieira <fabiovieira@id.uff.br>
2019-11-12 13:37:54 -0300

    Update growth_rate.R

--- growth_rate.R
+++ growth_rate.R
@@ -2,7 +2,7 @@ growth_rate <- function(year1, year2, pop_year1, pop_year2, monthly = FALSE){
   diff <- abs(year1 - year2)
   if(monthly == TRUE){
     if(diff == 1){
-      return((((pop_year2/pop_year1)^(1/diff)) - 1)/12)
+      return((((pop_year2/pop_year1)) - 1)/12)
     } else {
       return((((pop_year2/pop_year1)^(1/diff)) - 1)/12)
     }
