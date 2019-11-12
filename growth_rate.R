e2453a645fabf183f38b13807b109d913e5bf961
Fábio Vieira <fabiovieira@id.uff.br>
2019-11-12 12:03:14 -0300

    Update growth_rate.R

--- growth_rate.R
+++ growth_rate.R
@@ -1,8 +1,16 @@
-growth_rate <- function(year1, year2, pop_year1, pop_year2){
+growth_rate <- function(year1, year2, pop_year1, pop_year2, monthly = FALSE){
   diff <- abs(year1 - year2)
-  if(diff == 1){
-    return((pop_year2/pop_year1) - 1)
+  if(monthly == TRUE){
+    if(diff == 1){
+      return((((pop_year2/pop_year1)^(1/diff)) - 1)/12)
+    } else {
+      return((((pop_year2/pop_year1)^(1/diff)) - 1)/12)
+    }
   } else {
-    return(((pop_year2/pop_year1)^(1/diff)) - 1)
-  }
+      if(diff == 1){
+        return((pop_year2/pop_year1) - 1)
+      } else {
+        return(((pop_year2/pop_year1)^(1/diff)) - 1)
+      }
+  } 
 }
